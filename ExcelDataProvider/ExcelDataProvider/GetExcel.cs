ffb32af0aecae9dc0c078b354520cd678bda9e91
Jim Doyle <indellibleink@gmail.com>
2020-01-05 10:30:49 -0500

    Can get xls wrkbk cell data

--- ExcelDataProvider/ExcelDataProvider/GetExcel.cs
+++ ExcelDataProvider/ExcelDataProvider/GetExcel.cs
@@ -1,5 +1,6 @@
 using ExcelDataReader;
 using NUnit.Framework;
+using OfficeOpenXml;
 using System;
 using System.Data;
 using System.IO;
@@ -10,7 +11,7 @@ namespace ExcelDataProvider
     {
 
         [Test]
-        public void GetExcelFile()
+        public void UsingExcelDataReader()
         {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
             using (FileStream xlsFile = File.Open("D:\\repository\\SDET\\javaSDET\\ExcelFiles\\excelDriven.xlsx", FileMode.Open, FileAccess.Read))
@@ -20,8 +21,8 @@ namespace ExcelDataProvider
                     do
                     {
                         while (sheet.Read()) //Each ROW 
-                                              // At end of Row will Return False 
-                                              // and End While loop.
+                                             // At end of Row will Return False 
+                                             // and End While loop.
                         {
                             for (int column = 0; column < sheet.FieldCount; column++)
                             {
@@ -30,9 +31,48 @@ namespace ExcelDataProvider
                             }
                         }
                     } while (sheet.NextResult()); //Move to NEXT SHEET
-                                                   // At end of Row will Return False 
-                                                   // and End While loop.
+                                                  // At end of Row will Return False 
+                                                  // and End While loop.
 
+                }
+            }
+        }
+
+        [Test]
+        public void UsingEPPlus()
+        {
+            using (ExcelPackage xlsFile = new ExcelPackage(new FileInfo("D:\\repository\\SDET\\javaSDET\\ExcelFiles\\excelDriven.xlsx")))
+            {
+                ExcelWorksheet firstSheet = xlsFile.Workbook.Worksheets["CareerDevs"];
+                Console.WriteLine("Sheet 1 Data");
+                Console.WriteLine($"Cell A2 Value   : {firstSheet.Cells["A2"].Text}");
+                Console.WriteLine($"Cell A2 Color   : {firstSheet.Cells["A2"].Style.Font.Color.LookupColor()}");
+                Console.WriteLine($"Cell B2 Formula : {firstSheet.Cells["B2"].Formula}");
+                Console.WriteLine($"Cell B2 Value   : {firstSheet.Cells["B2"].Text}");
+                Console.WriteLine($"Cell B2 Border  : {firstSheet.Cells["B2"].Style.Border.Top.Style}");
+                Console.WriteLine("");
+
+                ExcelWorksheet secondSheet = xlsFile.Workbook.Worksheets["Data"];
+                Console.WriteLine($"Sheet 2 Data");
+                Console.WriteLine($"Cell A2 Formula : {secondSheet.Cells["A2"].Formula}");
+                Console.WriteLine($"Cell A2 Value   : {secondSheet.Cells["A2"].Text}");
+            }
+        }
+
+        [Test]
+        public void WorkingWithExcelDataReader()
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            using (FileStream xlsFile = File.Open("D:\\repository\\SDET\\javaSDET\\ExcelFiles\\excelDriven.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                using (IExcelDataReader sheet = ExcelReaderFactory.CreateReader(xlsFile))
+                {
+                    sheet.Read(); //grabs first sheet first Row: 1
+                   // Console.WriteLine(sheet.GetValue(0)); //  Output Row 1 Cell A
+                    sheet.NextResult(); // grabs Next Sheet
+                    sheet.Read(); // get Row 1 and move onto
+                    sheet.Read(); // to get Row 2.
+                    Console.WriteLine(sheet.GetValue(1));  // Output Row 2 Cell B
                 }
             }
         }
